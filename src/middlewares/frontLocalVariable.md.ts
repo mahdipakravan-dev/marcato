029bb0fdec532ee902b4377ff344f19f9ce32724
engpakravan <mahdipakravan79@gmail.com>
2021-01-10 19:04:37 +0330

    Change Pics

--- src/middlewares/frontLocalVariable.md.ts
+++ src/middlewares/frontLocalVariable.md.ts
@@ -1,8 +1,5 @@
 import { NextFunction , Request , Response } from "express";
-import Jwt from "../helpers/jwt";
 import config from 'config'
-
-import { DashboardToken } from '../helpers/interfaces'
 
 export default function(req:Request , res:Response , next:NextFunction){
   req.app.locals.projectName = config.get("projectName") || "مارکاتو"
