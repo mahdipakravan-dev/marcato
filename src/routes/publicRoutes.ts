d3fcbf5bb8beed3dff59c9b4a6da0018c1e52e4a
engpakravan <mahdipakravan79@gmail.com>
2021-01-10 14:52:46 +0330

    Change ThemeColor v1

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -4,8 +4,11 @@ import validationMiddleware from '../middlewares/validation.md'
 import SignUpDto from '../dto/signup.dto'
 import SignInDto from '../dto/signIn.dto'
 import homeController from '../controllers/home.controller'
+import frontLocalVariableMd from '../middlewares/frontLocalVariable.md'
 
 const Router = express.Router()
+
+Router.use(frontLocalVariableMd)
 
 Router.get('/' , homeController.getHome)
 
