50e0ff92172a8dc56cbcbf806ca11a609f00ccd8
engpakravan <mahdipakravan79@gmail.com>
2021-01-27 11:53:46 +0330

    HomePage Queries Implemented

--- src/controllers/frontend/frontend_home.ts
+++ src/controllers/frontend/frontend_home.ts
@@ -4,10 +4,10 @@ import { ProductModel } from "../../models/product";
 export default new class HomeController {
 
     public async getHome(req: Request, res: Response, next: NextFunction): Promise<void> {
-        const newProducts = await new ProductModel().Find({}) , 
-        topProducts = await new ProductModel().Find({}) , 
-        newGuitars = await new ProductModel().Find({}) , 
-        newKalimbas = await new ProductModel().Find({})
+        const newProducts = await new ProductModel().Sort({} , {created_at : -1} , 10) , 
+        topProducts = await new ProductModel().Sort({} , {sellCount : -1} , 10) , 
+        newGuitars = await new ProductModel().Find({'instrument.enName' : 'guitar'}) , 
+        newKalimbas = await new ProductModel().Find({'instrument.enName' : 'kalimba'})
         res.render('home' , {newProducts , topProducts , newGuitars , newKalimbas})
     }
 
